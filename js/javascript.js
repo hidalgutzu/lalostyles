12ce0d40a4128d0026864ede91aabba8cee0fe4b
Daniel Hidalgo <hidalgutzu@gmail.com>
2019-11-01 20:27:22 -0500

    home js mobile
    se removió el scroll al accionar el menu (.hideoverflow)

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,4 @@
+const body = document.querySelector('.body');
 const menu = document.querySelector('.burger-menu');
 const burgerButton = document.querySelector('#burger-menu');
 const closeButton = document.querySelector('#close-menu');
@@ -7,8 +8,10 @@ closeButton.addEventListener('click', hide)
 
 function show() {
     menu.classList.add('is-active');
+    body.classList.add('hideoverflow');
 }
 
 function hide() {
     menu.classList.remove('is-active');
+    body.classList.remove('hideoverflow');
 }
