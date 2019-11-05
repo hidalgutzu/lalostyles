c51021267f3ecc1747913ae406451b33813bfc3f
Daniel Hidalgo <hidalgutzu@gmail.com>
2019-11-05 17:05:25 -0500

    reviews js
    funcionamiento de reviews con js - "no se ha logrado hacer un bucle infinito"

--- js/javascript.js
+++ js/javascript.js
@@ -1,17 +1,43 @@
+// ---------- MENÚ HAMBURGUESA ----------
+   // llamamos a body para poder modificarlo
 const body = document.querySelector('.body');
+   // llamamos al menú
 const menu = document.querySelector('.burger-menu');
-const burgerButton = document.querySelector('#burger-menu');
+   // llamamos al botón "hamburguesa"
+const burgerButton = document.querySelector('.burger-button');
+   // llamamos al botón "cerrar"
 const closeButton = document.querySelector('#close-menu');
 
+   // CLICK para activar las funciones  
 burgerButton.addEventListener('click', show)
 closeButton.addEventListener('click', hide)
 
+   // la función ADD
 function show() {
     menu.classList.add('is-active');
     body.classList.add('hideoverflow');
 }
-
+   // la función REMOVE
 function hide() {
     menu.classList.remove('is-active');
     body.classList.remove('hideoverflow');
 }
+
+// ---------- AUTOPLAY RADIOBUTTONS ----------
+
+  var reviews = function() {
+    var i = 0;
+    while (i < 5) {
+      (function(i) {
+        setTimeout(function() {
+            document.querySelector('.slider__nav:nth-of-type'+'('+i+')').checked = true;
+        }, 2000 * i);
+      })(i++)
+    }
+  }
+reviews();
+
+
+  
+
+  
